561dc72b301d007abc9adb9bdc2f2abb9aa4e003
Pog-Frog <60003686+Pog-Frog@users.noreply.github.com>
2022-03-26 00:59:22 +0200

    initial commit

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def get_equation(request):
+    equation = request.GET['input']
+    if "|" in equation:
+        idx = equation.index("|")
+        
+    return HttpResponse(equation)
